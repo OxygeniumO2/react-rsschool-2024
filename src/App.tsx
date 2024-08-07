8030acf454c4fb28109e6412ec1aecf830487ac6
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-08-07 19:35:53 +0300

    remove: useless test, clear app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,6 @@
-import { createContext, useState } from 'react';
-import { SearchBar } from './components/SearchBar/SearchBar';
-import { Flyout } from './components/flyout/Flyout';
+import { createContext } from 'react';
 
 export const themeContext = createContext({
   theme: 'light',
   changeTheme: () => {},
 });
-
-const App = () => {
-  const [theme, setTheme] = useState('light');
-
-  return (
-    <div data-testid="app" className={`app ${theme}`}>
-      <div className="container">
-        <button
-          className="themeBtn"
-          onClick={() => {
-            setTheme(theme === 'light' ? 'dark' : 'light');
-          }}
-        >
-          {theme.toUpperCase()}
-        </button>
-        <img className="logo" src="/naruto-logo.png" alt="naruto" />
-        <SearchBar />
-
-        <Flyout />
-      </div>
-    </div>
-  );
-};
-
-export default App;
