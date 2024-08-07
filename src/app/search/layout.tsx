cec35c36ab30feb63591200a2f04155646bf906d
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-08-07 15:02:29 +0300

    feat: error boundary added

--- src/app/search/layout.tsx
+++ src/app/search/layout.tsx
@@ -4,19 +4,23 @@ import { themeContext } from '../../App';
 import { ChangeTheme } from '../../components/changeTheme/ChangeTheme';
 import { SearchBar } from '../../components/SearchBar/SearchBar';
 import { Flyout } from '../../components/flyout/Flyout';
+import ErrorBoundary from '../../components/ErrorBoundary/ErrorBoundary';
+import { ErrorComponent } from '../../components/ErrorBoundary/ErrorComponent/ErrorComponent';
 
 export default function SearchLayout({ children }: { children: ReactNode }) {
   const { theme } = useContext(themeContext);
 
   return (
-    <div data-testid="app" className={`app ${theme}`}>
-      <div className="container">
-        <ChangeTheme />{' '}
-        <img className="logo" src="/naruto-logo.png" alt="naruto" />
-        <SearchBar />
-        {children}
-        <Flyout />
+    <ErrorBoundary fallback={<ErrorComponent />}>
+      <div data-testid="app" className={`app ${theme}`}>
+        <div className="container">
+          <ChangeTheme />{' '}
+          <img className="logo" src="/naruto-logo.png" alt="naruto" />
+          <SearchBar />
+          {children}
+          <Flyout />
+        </div>
       </div>
-    </div>
+    </ErrorBoundary>
   );
 }
