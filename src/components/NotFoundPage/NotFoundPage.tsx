2340909bf930e9d63a5b3837f141e6b43faa7e65
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-08-11 13:45:33 +0300

    fix: styles 404 page

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export const NotFoundPage = () => {
   return (
-    <div>
-      <h2>404 Page Not Found</h2>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h2 style={{ marginBottom: '20px' }}>404 Page Not Found</h2>
       <Link href={'/search/name=""/1'}>To main</Link>
     </div>
   );
